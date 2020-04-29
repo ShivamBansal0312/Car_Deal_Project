51a7e4ba7e5b345f2d5ca087ac465c1fca68b270
pranjalguptabest <41382798+pranjalguptabest@users.noreply.github.com>
2020-04-29 15:43:17 +0530

    Add files via upload

---
+++ RoleDetails/src/main/java/com/cts/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.cts.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.cts.model.RoleModel;
+
+public interface RoleRepository extends CrudRepository<RoleModel,Long>{
+
+}
