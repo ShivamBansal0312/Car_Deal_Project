ab59e2e7cb952bbc0bc0fd83723ed53b0ddca529
ShivamBansal0312 <bsvm312@gmail.com>
2020-05-07 18:06:36 +0530

    Some Changes

--- RoleDetails/src/main/java/com/cts/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cts.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.cts.model.RoleModel;
-
-public interface RoleRepository extends CrudRepository<RoleModel,Long>{
-
-}
