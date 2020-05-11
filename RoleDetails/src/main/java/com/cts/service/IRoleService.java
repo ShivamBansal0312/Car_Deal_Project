fdbf3fc34f758c12f99601f52fcdb17708049f92
Pratyush4456 <60208775+Pratyush4456@users.noreply.github.com>
2020-05-11 11:54:15 +0530

    login model and junit tests for get and post

--- RoleDetails/src/main/java/com/cts/service/IRoleService.java
+++ RoleDetails/src/main/java/com/cts/service/IRoleService.java
@@ -2,13 +2,16 @@ package com.cts.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.cts.model.LoginModel;
 import com.cts.model.RoleModel;
 
+@Service
 public interface IRoleService {
 	
 	public List<RoleModel> getAllRoleDetails();
 	public void saveRoleDetails(RoleModel roleModel);
-	public List<RoleModel> matchRole(LoginModel loginModel);
+	public String matchRole(LoginModel loginModel);
 
 }
--- RoleDetails/src/main/java/com/cts/service/RoleService.java
+++ RoleDetails/src/main/java/com/cts/service/RoleService.java
@@ -1,6 +1,7 @@
 package com.cts.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.cts.model.LoginModel;
 import com.cts.model.RoleModel;
+import com.cts.repository.LoginRepository;
 import com.cts.repository.RoleRepository;
 
 @Service
@@ -17,6 +19,9 @@ public class RoleService implements IRoleService{
 	@Autowired
 	RoleRepository roleRepository;
 	
+	@Autowired
+	LoginRepository loginRepository;
+	
 	public List<RoleModel> getAllRoleDetails(){
 		return (List<RoleModel>) roleRepository.findAll();
 	}
@@ -24,7 +29,35 @@ public class RoleService implements IRoleService{
 	public void saveRoleDetails(RoleModel roleModel) {
 		roleRepository.save(roleModel);
 	}
-	public List<RoleModel> matchRole(LoginModel loginModel){
-		return roleRepository.matchRole(loginModel);
+	public String matchRole(LoginModel loginModel){
+		LoginModel role;
+		Optional<LoginModel> proxy = loginRepository.findById(loginModel.getUserId());
+	     if(proxy.isPresent()) {
+		role = proxy.get();
+		if(role.getPassword().equals(loginModel.getPassword())){
+			if(role.getUserType().equals("user")) {
+				return "welcome user";
+			}else {
+				return "welcome admin";
+			}
+			
+		}else {
+			return "Password Didn't Match";
+		}
+	}else {
+		return "UserId is Not Present";
 	}
+//	     if(role.getPassword()==loginModel.getPassword()){
+////	    	 if(role.getUserType()=="user") {
+////	    		 return "User";
+////	    	 }else {
+////	    		 return "Admin";
+////	    	 }
+//	    	 return "welcome";
+//	     }else {
+//	    	 return "Password Didn't Match";
+//	     }
+	
+
+}
 }
